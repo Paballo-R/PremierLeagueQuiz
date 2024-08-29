810079484e8f737c89d487b833a9257bb70722c0
Paballo Mampshika <paballo@Paballos-MacBook-Pro.local>
2024-08-29 19:24:33 +0200

    initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,95 @@
+export const data = [
+    {
+        question: "Which team won the 23/24 Premier League title?",
+        option1: "Arsenal",
+        option2: "Liverpool",
+        option3: "Manchester City",
+        option4: "Aston Villa",
+        ans: 3,
+
+
+    },
+    {
+        question: "Who was the top scorer in the 23/24 Premier League season?",
+        option1: "Erling Haaland",
+        option2: "Mohammed Salah",
+        option3: "Ollie Watkins",
+        option4: "Cole Palmer",
+        ans: 1,
+
+
+    },
+    {
+        question: "Who is the all time leading goalscorer in Premier League history?",
+        option1: "Alan Shearer",
+        option2: "Wayne Rooney",
+        option3: "Harry Kane",
+        option4: "Cristiano Ronaldo",
+        ans: 1,
+
+
+    },
+    {
+        question: "Who has the most assists in Premier League history?",
+        option1: "Kevin De Bruyne",
+        option2: "Cesc Fabregas",
+        option3: "Wayne Rooney",
+        option4: "Ryan Giggs",
+        ans: 4,
+
+    },
+    {
+        question: "Who had the most clean sheets in the 23/24 Premier League season?",
+        option1: "Ederson de Moraes",
+        option2: "Nick Pope",
+        option3: "David Raya",
+        option4: "Emiliano Martinez",
+        ans: 3,
+
+    },
+    {
+        question: "Who has the most appearances in Premier League history?",
+        option1: "James Milner",
+        option2: "Gareth Barry",
+        option3: "Frank Lampard",
+        option4: "Ryan Giggs",
+        ans: 2,
+
+    },
+    {
+        question: "Which player had the most assists in the 23/24 Premier League season?",
+        option1: "Kevin De Bruyne",
+        option2: "Ollie Watkins",
+        option3: "Cole Palmer",
+        option4: "Morgan Gibbs-White",
+        ans: 2,
+
+    },
+    {
+        question: "Which player has the most passes in Premier League history?",
+        option1: "Jordan Henderson",
+        option2: "Kyle Walker",
+        option3: "Gareth Barry",
+        option4: "Ryan Giggs",
+        ans: 1,
+
+    },
+    {
+        question: "Which player has hit the woodwork the most in Premier League history?",
+        option1: "Kevin De Bruyne",
+        option2: "Sergio Aguero",
+        option3: "Robin Van Persie",
+        option4: "Harry Kane",
+        ans: 3,
+
+    },
+    {
+        question: "Which team has won the most Premier League titles ever?",
+        option1: "Liverpool",
+        option2: "Blackburn Rovers",
+        option3: "Chelsea",
+        option4: "Manchester United",
+        ans: 4,
+
+    },
+]
